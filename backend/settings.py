b427935e795156d5cfae4f7d21988fc0cf3002ba
shtrih2k4 <andriizhevahin@gmail.com>
2024-04-29 21:11:13 +0300

    Added CORS support

--- backend/settings.py
+++ backend/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     # Libs, frameworks, etc.
     "rest_framework",
     'drf_yasg',
+    #CORS
+    'corsheaders',
     # Apps
     "employees",
     "files",
@@ -55,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -139,3 +142,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# CORS Settings
+CORS_ALLOW_ALL_ORIGINS = True
